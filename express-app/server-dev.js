8e47fbaf0773b7f005353c4858f1494a72b993e4
PatientZero <amarscn@outlook.com>
2020-03-31 11:02:04 -0400

    Update server file name

---
+++ express-app/server-dev.js
@@ -0,0 +1,92 @@
+const express       = require('express');
+const session       = require('express-session');
+const bodyParser    = require('body-parser');
+const mysql         = require('mysql');
+const path          = require('path');
+
+var app     = express();
+var http = require("http").Server(app);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+    res.render('home');
+});
+app.get('/form',(req,res)=>{
+  res.render('index');
+})
+app.post('/report.symptom',(req,res)=>{
+  var user_guid = req.body.user_guid;
+  var member_id = req.body.member_id;
+  var dry_cough = req.body.dry_cough;
+  var pneumonia = req.body.pneumonia;
+  var difficulty_breathing  = req.body.difficulty_breathing;
+  var difficulty_walking  = req.body.difficulty_walking;
+  var appetite  = req.body.appetite;
+  var diarrhea  = req.body.diarrhea;
+  var muscle_ache = req.body.muscle_ache;
+  var fatigue = req.body.fatigue;
+  var runny_nose  = req.body.runny_nose;
+  var congestion  = req.body.congestion;
+  var sore_throat = req.body.sore_throat;
+  var fever_c     = req.body.fever_c;
+  var headache    = req.body.headache;
+  var confusion_dizziness = req.body.confusion_dizziness;
+  var nausea  = req.body.nausea;
+  var chills  = req.body.chills;
+  var other_pain  = req.body.other_pain;
+});
+
+app.post('/report.testing',(req,res)=>{
+  var user_guid = req.body.user_guid;
+  var tested  = req.body.tested;
+  var recovered = req.body.recovered;
+  var hospitalized  = req.body.hospitalized;
+  var ventilation   = req.body.ventilation;
+  var oxygen        = req.body.oxygen;
+  var other_symptoms  = req.body.other_symptoms;
+
+});
+
+app.post('/report.transmission',(req,res)=>{
+  var user_guid = req.body.user_guid;
+  var travel_amount = req.body.travel_amount;
+  var travel_distance = req.body.travel_distance;
+  var hospitalized  = req.body.hospitalized;
+  var surface_touch = req.body.surface_touch;
+  var number_of_regular_contacts  = req.body.number_of_regular_contacts;
+
+});
+
+app.post('/register.user',(req,res)=>{
+  var user_guid = req.body.user_guid;
+  var passcode  = req.body.passcode;
+  var user_guid_value = req.body.user_guid_value;
+  var passcode_value  = req.body.passcode_value;
+
+});
+
+app.post('/authenticate.user',(req,res)=>{
+  var user_guid = req.body.user_guid;
+  var user_guid_value = req.body.user_guid_value;
+  var passcode        = req.body.passcode;
+  var passcode_value  = req.body.passcode_value;
+
+});
+
+app.post('/register.location',(req,res)=>{
+  var user_guid = req.body.user_guid;
+  var user_guid_value = req.body.user_guid_value;
+  var passcode  = req.body.passcode;
+  var passcode_value  = req.body.passcode_value;
+  
+});
+const PORT = process.env.PORT || 10000;
+
+// port to listen
+http.listen(PORT, function(){
+  console.log('listening on port: ', PORT);
+});
