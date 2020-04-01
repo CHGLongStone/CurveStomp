3abac975b1472744c8d32b828a8e1c1120fdd275
Amardeep Singh <amarscn@outlook.com>
2020-03-31 22:09:15 -0400

    More changes to server and view

--- express-app/assets/js/form2.js
+++ express-app/assets/js/form2.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-$('#usr_total_members').focusout(function(){
-    var totalusr    = $('#usr_total_members').val();
-    document.getElementById("slider_value2").innerHTML=totalusr;
-    for(var i=0;i<totalusr;i++)
-    {
-        $("<div id='mydiv' class='notice'>Age:<input type='text'>Gender:<select id='gender'><option value='1'>Male</option><option value='2'>Female</option></select></div>").appendTo("#memberdata");
+$(document).ready(function () {
+    $('#usr_identity').click(function(){
+        $('#usr_identity').val(create_UUID());
+    })
+    $('#usr_total_members').focusout(function () {
+        var totalusr = $('#usr_total_members').val();
+        
+        document.getElementById("slider_value2").innerHTML = totalusr;
+        for (var i = 0; i < totalusr; i++) {
+            $("<div id='mydiv' class='notice'>Age:<input type='text'>Gender:<select id='gender'><option value='1'>Male</option><option value='2'>Female</option></select></div>").appendTo("#memberdata");
 
+        }
+
+
+    });
+   
+    function create_UUID(){
+        var dt = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (dt + Math.random()*16)%16 | 0;
+            dt = Math.floor(dt/16);
+            return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        });
+        return uuid;
     }
-    
-    
-});
 })
