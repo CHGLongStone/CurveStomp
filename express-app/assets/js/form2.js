7fe92cb25ff30b47963dcfd67f1f72bb0c31950c
Amardeep Singh <amarscn@outlook.com>
2020-04-03 22:24:20 -0400

    Ejs view and JS changes

--- express-app/assets/js/form2.js
+++ express-app/assets/js/form2.js
@@ -8,10 +8,12 @@ $(document).ready(function () {
     })
     $('#h_mem_add').click(function(){
         var id='h_member_row'+i;
-        $('#h_member_row').clone(true,true).attr({"id":id,"class":"collapsible"}).insertAfter('#h_member_row');
+        var id2 = 'member'+i;
+        $('#h_member_row').clone(true,true).attr({"id":id}).insertAfter('#h_member_row');
+        $('#member').clone(true,true).attr({"id":id2}).insertAfter('#member');
         i=i+1;        
     })
-    
+
     $('#h_id_pass_confirm').change(function(){
         var usrpass = $('#h_id_pass').val();
         var usrpasscnf  = $('#h_id_pass_confirm').val();
