e86ad08e89ade54821c7261f47262facfd60b123
Resident Six <gale.josh@gmail.com>
2020-04-05 14:06:43 -0400

    Member List Functionality

--- express-app/assets/js/form.js
+++ express-app/assets/js/form.js
@@ -1,3 +1,22 @@
+let form_data = {
+    "household": {
+        "identity": {
+            "email": null,
+            "unique_identifier": null,
+            "passcode": null,
+            "confirm_passcode": null
+        },
+        "location": {
+            "country": null,
+            "region": null,
+            "city": null,
+            "street_name": null,
+            "postal_code": null
+        }
+    },
+    "members": {}
+};
+
 $(document).ready(function () {
     var i = 1;
     var usrobj = {};
@@ -49,16 +68,14 @@ $(document).ready(function () {
         var alias = $('#h_member_row').find('#h_mem_alias').val();
         if (age == null || age == '') {
             $('#h_member_row').find('#h_mem_age').css('border-color', 'red');
-           
+
         } else {
             $('#h_member_row').find('#h_mem_age').css('border-color', 'green');
         }
-        if(alias==null || alias=='')
-        {
+        if (alias == null || alias == '') {
             $('#h_member_row').find('#h_mem_alias').css('border-color', 'red');
-            
-        }
-        else{
+
+        } else {
             $('#h_member_row').find('#h_mem_alias').css('border-color', 'green');
         }
         $('#h_member_row').find('legend').html(age + gender + '-' + alias);
@@ -66,7 +83,7 @@ $(document).ready(function () {
 
     $('#h_mem_add').click(function () {
         var id = 'h_member_row' + i;
-        $('#h_member_row').clone(false,false).attr({ "id": id }).insertAfter('#h_member_row');
+        $('#h_member_row').clone(false, false).attr({"id": id}).insertAfter('#h_member_row');
         i = i + 1;
     });
 
@@ -87,22 +104,22 @@ $(document).ready(function () {
             $('#h_loc_country').css('border-color', 'red');
         }
     })
-    $('#h_loc_city').focusout(()=>{
+    $('#h_loc_city').focusout(() => {
         if (location['city'] == null) {
             $('#h_loc_city').css('border-color', 'red');
         }
     })
-    $('#h_loc_region').focusout(()=>{
+    $('#h_loc_region').focusout(() => {
         if (location['region'] == null) {
             $('#h_loc_region').css('border-color', 'red');
         }
     })
-    $('#h_loc_street').focusout(()=>{
+    $('#h_loc_street').focusout(() => {
         if (location['street_name'] == null) {
             $('#h_loc_street').css('border-color', 'red');
         }
     })
-    $('#h_loc_pcode').focusout(()=>{
+    $('#h_loc_pcode').focusout(() => {
         if (location['postal_code'] == null) {
             $('#h_loc_pcode').css('border-color', 'red');
         }
@@ -111,8 +128,7 @@ $(document).ready(function () {
         location['country'] = $('#h_loc_country').val();
         if (location['country'] == null) {
             $('#h_loc_country').css('border-color', 'red');
-        }
-        else {
+        } else {
             $('#h_loc_country').css('border-color', 'green');
         }
     })
@@ -120,8 +136,7 @@ $(document).ready(function () {
         location['city'] = $('#h_loc_city').val();
         if (location['city'] == null) {
             $('#h_loc_city').css('border-color', 'red');
-        }
-        else {
+        } else {
             $('#h_loc_city').css('border-color', 'green');
         }
     })
@@ -129,8 +144,7 @@ $(document).ready(function () {
         location['region'] = $('#h_loc_region').val();
         if (location['region'] == null) {
             $('#h_loc_region').css('border-color', 'red');
-        }
-        else {
+        } else {
             $('#h_loc_region').css('border-color', 'green');
         }
     })
@@ -139,8 +153,7 @@ $(document).ready(function () {
         location['postal_code'] = $('#h_loc_pcode').val();
         if (location['postal_code'] == null) {
             $('#h_loc_pcode').css('border-color', 'red');
-        }
-        else {
+        } else {
             $('#h_loc_pcode').css('border-color', 'green');
         }
     })
@@ -148,24 +161,19 @@ $(document).ready(function () {
         location['street_name'] = $('#h_loc_street').val();
         if (location['street_name'] == null) {
             $('#h_loc_street').css('border-color', 'red');
-        }
-        else {
+        } else {
             $('#h_loc_street').css('border-color', 'green');
         }
     })
 
-    $('#h_member_row').find('#h_mem_age').on('input',()=>{
+    $('#h_member_row').find('#h_mem_age').on('input', () => {
         var mage = $('#h_member_row').find('#h_mem_age').val();
-        if(mage==null)
-        {
-            $('#h_member_row').find('#h_mem_age').css('border-color','red');
-        }
-        else
-        {
-            $('#h_member_row').find('#h_mem_age').css('border-color','green');
+        if (mage == null) {
+            $('#h_member_row').find('#h_mem_age').css('border-color', 'red');
+        } else {
+            $('#h_member_row').find('#h_mem_age').css('border-color', 'green');
         }
     })
-
 
 
     $('#btnSubmit').click(function (e) {
@@ -234,4 +242,96 @@ $(document).ready(function () {
         });
         return uuid;
     }
+
+
+    // Add a new member row when 'add new member' is clicked
+    function saveMemberRow(memb_row) {
+
+        memb_row = $(memb_row);
+
+        // Grab input values
+        let m_age = memb_row.find('#h_mem_age').val();
+        let m_sex = memb_row.find('#h_mem_bio_gender').val();
+        let m_alias = memb_row.find('#h_mem_alias').val().toUpperCase();
+
+        // TODO: Validate input values
+        if (([m_age, m_sex].includes(null) || [m_age, m_sex].includes(''))) {
+            console.log("INVALID MEMBER ROW: " + m_age + ',' + m_sex + ',' + m_alias);
+            return false;
+        }
+        let memb_id = m_age + m_sex + '-' + m_alias;
+        if (memb_id in form_data['members']) {
+            console.log("DUPLICATE MEMBER EXISTS: " + memb_id);
+            return false;
+        }
+        let prev_memb_id = memb_row.find('legend').html();
+        if (prev_memb_id != "AAAS-NN") {
+            console.log("Swapping: " + prev_memb_id);
+            // We are altering a member. Preserve all data, then remove.
+            form_data['members'][memb_id] = form_data['members'][prev_memb_id];
+            delete form_data['members'][prev_memb_id];
+        } else {
+            form_data['members'][memb_id] = {};
+        }
+
+        // Save data.
+        form_data['members'][memb_id]['age'] = m_age;
+        form_data['members'][memb_id]['sex'] = m_sex;
+        form_data['members'][memb_id]['alias'] = m_alias;
+
+        // Update UI
+        memb_row.find('legend').html(memb_id);
+        return true;
+    }
+
+    function delMember(memb_row) {
+        // TODO: prompt user for confirmation before deleting
+        memb_row = $(memb_row);
+
+        let prev_memb_id = memb_row.find('legend').html();
+        if (prev_memb_id == "AAAS-NN") {
+            // We are altering a member. Preserve all data, then remove.
+            memb_row.attr({"id": 'AAAS-NN'});
+            memb_row.find('#h_mem_age').val(null);
+            memb_row.find('#h_mem_sex').val(null);
+            memb_row.find('#h_mem_alias').val(null);
+        } else {
+            let m_age = memb_row.find('#h_mem_age').val();
+            let m_sex = memb_row.find('#h_mem_bio_gender').val();
+            let m_alias = memb_row.find('#h_mem_alias').val().toUpperCase();
+            let memb_id = m_age + m_sex + '-' + m_alias;
+            if (memb_id in form_data['members']) {
+                delete form_data['members'][memb_id];
+            }
+            memb_row.remove();
+        }
+    }
+
+    $('#h_mem_save').click((e) => {
+        saveMemberRow(e.target.parentNode.parentNode.parentNode);
+    });
+
+    $('#h_mem_del').click((e) => {
+        delMember(e.target.parentNode.parentNode.parentNode)
+    });
+
+    $('#h_mem_add').click(() => {
+        let prv = $('fieldset.h_member_row:nth-last-of-type(1)');
+        let nxt = prv;
+        if (saveMemberRow(prv)) {
+            nxt = prv.clone().insertAfter(prv);
+            nxt.find('#h_mem_save').click((e) => {
+                saveMemberRow(e.target.parentNode.parentNode.parentNode);
+            });
+            nxt.find('#h_mem_del').click((e) => {
+                delMember(e.target.parentNode.parentNode.parentNode);
+            });
+        }
+        nxt.attr({"id": 'AAAS-NN'});
+        nxt.find('#h_mem_age').val(null);
+        nxt.find('#h_mem_sex').val(null);
+        nxt.find('#h_mem_alias').val(null);
+        nxt.find('legend').html("AAAS-NN");
+        return prv;
+    });
 });
