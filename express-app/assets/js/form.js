3f963f6d6bedb8eef169e3897a41e24e6ed5b487
Amardeep Singh <amarscn@outlook.com>
2020-04-05 01:56:13 -0400

    Client Side validation changes to form.js

--- express-app/assets/js/form.js
+++ express-app/assets/js/form.js
@@ -5,15 +5,17 @@ $(document).ready(function () {
     var identity = {};
     var location = {};
     var members = {};
-  
+    var symptoms = {};
+    var transmissions = {};
+    var clinical_findings = {};
 
-    $('#h_id_load').click(() =>{
-        var exidn  = {};
-        exidn['identity']  = $('#h_id_uid').val();
-        exidn['pass']    = $('#h_id_pass').val();
-       
-        if(exidn['identity']!=null && exidn['pass']!=null)
-        {
+
+    $('#h_id_load').click(() => {
+        var exidn = {};
+        exidn['identity'] = $('#h_id_uid').val();
+        exidn['pass'] = $('#h_id_pass').val();
+
+        if (exidn['identity'] != null && exidn['pass'] != null) {
             $.ajax({
                 type: "POST",
                 url: "/login",
@@ -47,15 +49,24 @@ $(document).ready(function () {
         var alias = $('#h_member_row').find('#h_mem_alias').val();
         if (age == null || age == '') {
             $('#h_member_row').find('#h_mem_age').css('border-color', 'red');
+           
         } else {
             $('#h_member_row').find('#h_mem_age').css('border-color', 'green');
+        }
+        if(alias==null || alias=='')
+        {
+            $('#h_member_row').find('#h_mem_alias').css('border-color', 'red');
+            
+        }
+        else{
+            $('#h_member_row').find('#h_mem_alias').css('border-color', 'green');
         }
         $('#h_member_row').find('legend').html(age + gender + '-' + alias);
     });
 
     $('#h_mem_add').click(function () {
         var id = 'h_member_row' + i;
-        $('#h_member_row').clone().attr({"id": id}).insertAfter('#h_member_row');
+        $('#h_member_row').clone(false,false).attr({ "id": id }).insertAfter('#h_member_row');
         i = i + 1;
     });
 
@@ -71,11 +82,96 @@ $(document).ready(function () {
         }
     })
 
+    $('#h_loc_country').focusout(() => {
+        if (location['country'] == null) {
+            $('#h_loc_country').css('border-color', 'red');
+        }
+    })
+    $('#h_loc_city').focusout(()=>{
+        if (location['city'] == null) {
+            $('#h_loc_city').css('border-color', 'red');
+        }
+    })
+    $('#h_loc_region').focusout(()=>{
+        if (location['region'] == null) {
+            $('#h_loc_region').css('border-color', 'red');
+        }
+    })
+    $('#h_loc_street').focusout(()=>{
+        if (location['street_name'] == null) {
+            $('#h_loc_street').css('border-color', 'red');
+        }
+    })
+    $('#h_loc_pcode').focusout(()=>{
+        if (location['postal_code'] == null) {
+            $('#h_loc_pcode').css('border-color', 'red');
+        }
+    })
+    $('#h_loc_country').on('input', () => {
+        location['country'] = $('#h_loc_country').val();
+        if (location['country'] == null) {
+            $('#h_loc_country').css('border-color', 'red');
+        }
+        else {
+            $('#h_loc_country').css('border-color', 'green');
+        }
+    })
+    $('#h_loc_city').on('input', () => {
+        location['city'] = $('#h_loc_city').val();
+        if (location['city'] == null) {
+            $('#h_loc_city').css('border-color', 'red');
+        }
+        else {
+            $('#h_loc_city').css('border-color', 'green');
+        }
+    })
+    $('#h_loc_region').on('input', () => {
+        location['region'] = $('#h_loc_region').val();
+        if (location['region'] == null) {
+            $('#h_loc_region').css('border-color', 'red');
+        }
+        else {
+            $('#h_loc_region').css('border-color', 'green');
+        }
+    })
+
+    $('#h_loc_pcode').on('input', () => {
+        location['postal_code'] = $('#h_loc_pcode').val();
+        if (location['postal_code'] == null) {
+            $('#h_loc_pcode').css('border-color', 'red');
+        }
+        else {
+            $('#h_loc_pcode').css('border-color', 'green');
+        }
+    })
+    $('#h_loc_street').on('input', () => {
+        location['street_name'] = $('#h_loc_street').val();
+        if (location['street_name'] == null) {
+            $('#h_loc_street').css('border-color', 'red');
+        }
+        else {
+            $('#h_loc_street').css('border-color', 'green');
+        }
+    })
+
+    $('#h_member_row').find('#h_mem_age').on('input',()=>{
+        var mage = $('#h_member_row').find('#h_mem_age').val();
+        if(mage==null)
+        {
+            $('#h_member_row').find('#h_mem_age').css('border-color','red');
+        }
+        else
+        {
+            $('#h_member_row').find('#h_mem_age').css('border-color','green');
+        }
+    })
+
+
+
     $('#btnSubmit').click(function (e) {
         identity['email'] = $('#h_id_email').val();
         identity['unique_id'] = $('#h_id_uid').val();
         identity['passcode'] = $('#h_id_pass').val();
-        location['country'] = $('#h_loc_country').val();
         location['region'] = $('#h_loc_region').val();
         location['city'] = $('#h_loc_city').val();
         location['street_name'] = $('#h_loc_street').val();
