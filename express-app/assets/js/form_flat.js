78119acea835ce5ea40f10a89cf26a61844b331c
Resident Six <gale.josh@gmail.com>
2020-04-07 22:38:55 -0400

    v1 functionality on flat files

--- express-app/assets/js/form_flat.js
+++ express-app/assets/js/form_flat.js
@@ -3,165 +3,257 @@ function load_user_data(user_id, passcode) {
     console.log("Attempting to load:" + user_id)
 }
 
+function saveMemberRow(memb_row) {
+
+    memb_row = $(memb_row);
+
+    // Grab input values
+    let m_age = memb_row.find('#h_mem_age').val();
+    let m_sex = memb_row.find('#h_mem_bio_gender').val();
+    let m_alias = memb_row.find('#h_mem_alias').val().toUpperCase();
+
+    // TODO: Validate input values
+    if (([m_age, m_sex].includes(null) || [m_age, m_sex].includes(''))) {
+        console.log("INVALID MEMBER ROW: " + m_age + ',' + m_sex + ',' + m_alias);
+        return false;
+    }
+
+    let memb_id = m_age + m_sex + '-' + m_alias;
+
+    if (memb_id in form_data['members']) {
+        console.log("DUPLICATE MEMBER EXISTS: " + memb_id);
+        return false;
+    }
+
+    let prev_memb_id = memb_row.find('legend').html();
+
+    if (prev_memb_id != "AAAS-NN") {
+        console.log("Swapping: " + prev_memb_id);
+        // We are altering a member. Preserve all data, then remove.
+        form_data['members'][memb_id] = form_data['members'][prev_memb_id];
+        delete form_data['members'][prev_memb_id];
+    } else {
+        form_data['members'][memb_id] = {};
+    }
+
+    // Save data.
+    form_data['members'][memb_id] = {
+        'age': m_age,
+        'sex': m_sex,
+        'alias': m_alias
+    };
+
+    // Update UI
+    memb_row.find('legend').html(memb_id);
+    memb_row.attr('id', memb_id);
+
+    let btn = memb_row.find('#h_mem_save');
+
+    btn.clone().attr('id', 'h_mem_delete').val('Delete').insertAfter(btn).click((e) => {
+        delMember(e.target.parentNode.parentNode.parentNode);
+    });
+    btn.clone().attr('id', 'h_mem_report').val('Report').insertAfter(btn).click(() => {
+        // Reference the current member in the section title
+        $('#m_cur_memcode').html(memb_id);
+
+        // Hide the household section
+        $('#household').hide();
+
+        // Reveal the member section
+        $('#member').show();
+    });
+
+    // Remove the "save" button from the member row
+    btn.remove();
+    return true;
+}
+
+function delMember(memb_row) {
+    // TODO: prompt user for confirmation before deleting
+    memb_row = $(memb_row);
+
+    let prev_memb_id = memb_row.find('legend').html();
+
+    if (prev_memb_id == "AAAS-NN") {
+        memb_row.attr("id", 'AAAS-NN');
+        memb_row.find('#h_mem_age').val(null);
+        memb_row.find('#h_mem_sex').val(null);
+        memb_row.find('#h_mem_alias').val(null);
+    } else {
+        // Wipe data from form_data
+        let m_age = memb_row.find('#h_mem_age').val();
+        let m_sex = memb_row.find('#h_mem_bio_gender').val();
+        let m_alias = memb_row.find('#h_mem_alias').val().toUpperCase();
+        let memb_id = m_age + m_sex + '-' + m_alias;
+        if (memb_id in form_data['members']) {
+            delete form_data['members'][memb_id];
+        }
+        // Make sure not to remove the last slot, so that users can enter new members
+        if (jQuery.isEmptyObject(form_data['members'])) {
+            memb_row.attr({"id": 'AAAS-NN'});
+            memb_row.find('#h_mem_age').val(null);
+            memb_row.find('#h_mem_sex').val(null);
+            memb_row.find('#h_mem_alias').val(null);
+            memb_row.find('legend').html("AAAS-NN");
+            memb_row.find('#h_mem_delete').remove();
+            memb_row.find('#h_mem_report').remove();
+        } else {
+            memb_row.remove();
+        }
+    }
+}
+
 let form_data = {
     "household": {
         "identity": {
-            "email": null,
-            "unique_identifier": null,
+            "unique_identifier": '123-456-789',
             "passcode": null,
             "confirm_passcode": null
         },
         "location": {
-            "country": null,
-            "region": null,
-            "city": null,
-            "street_name": null,
-            "postal_code": null
+            "country": 'Canada',
+            "region": "Ontario",
+            "city": "Ottawa",
+            "street_name": "Triangle St.",
+            "postal_code": "M8C 7J9"
         }
     },
-    "members": {}
+    "members": {
+        "38M-JG": {
+            "age": 38,
+            "sex": "M",
+            "alias": "JG"
+        },
+        "32F-VG": {
+            "age": 32,
+            "sex": "F",
+            "alias": "VG"
+        },
+        "1M-UG": {
+            "age": 1,
+            "sex": "M",
+            "alias": "UG"
+        },
+    }
 };
 
-$(document).ready(function () {
-    $('#member').toggle("hidden");
 
-    // Handle changes to household:identity:unique_identifier
-    $('#h_id_uid').change((e) => {
-        console.log(e.target.value + ',' + $('#h_id_pass').val());
-        load_user_data(e.target.value);
-        if (form_data['household']['identity']['unique_identifier'] == null) {
-            form_data['household']['identity']['unique_identifier'] = e.target.value;
+$(document).ready(function () {
+
+    // Make all fieldsets and H2 titled elements collapsible
+    for (let header of document.querySelectorAll("h2, section > fieldset > legend")) {
+        header.classList.add("collapsible");
+        header.addEventListener("click", () => {
+            Array.from(header.parentNode.children)
+                .filter(n => n !== header)
+                .forEach(n => n.classList.toggle("hidden"))
+        });
+        // Collapse by default:
+        // header.click()
+    }
+
+    // Hide member section
+    $('#member').hide();
+
+    // Expand Household section
+    $('fieldset#h_location').hide();
+    $('fieldset#h_members').hide();
+
+    // Hide confirm passcode field
+    $('#h_id_pass_confirm').parent().hide();
+
+    // Load existing profile TODO: UN-STUB.
+    $('#h_id_load').click(() => {
+        // Organize UI
+        $('form > .explainer').hide();
+        $('section > fieldset#h_identity > legend').click();
+        $('section > fieldset#h_location').show();
+        $('section > fieldset#h_location > legend').click();
+        $('section > fieldset#h_members').show();
+
+        // Load Identity information from data obj
+        $('#h_id').html(": " + form_data['household']['identity']['unique_identifier']);
+        $('#h_loc_country').val(form_data['household']['location']['country']);
+        $('#h_loc_region').val(form_data['household']['location']['region']);
+        $('#h_loc_city').val(form_data['household']['location']['city']);
+        $('#h_loc_street').val(form_data['household']['location']['street_name']);
+        $('#h_loc_pcode').val(form_data['household']['location']['postal_code']);
+
+        // Load Member information from data obj
+        for (let member of Object.keys(form_data['members'])) {
+            let m_row = $('fieldset.h_member_row:nth-last-of-type(1)');
+            let new_m_row = m_row.clone();
+            m_row.find("#h_mem_age").val(form_data['members'][member]['age']);
+            m_row.find("#h_mem_bio_gender").val(form_data['members'][member]['sex']);
+            m_row.find("#h_mem_alias").val(form_data['members'][member]['alias']);
+            m_row.find('legend').html(member);
+            m_row.attr('id', member);
+            let btn = m_row.find('#h_mem_save');
+            btn.clone().attr('id', 'h_mem_delete').val('Delete').insertAfter(btn).click((e) => {
+                delMember(e.target.parentNode.parentNode.parentNode);
+            });
+            btn.clone().attr('id', 'h_mem_report').val('Report').insertAfter(btn).click(() => {
+                // Reference the current member in the section title
+                $('#m_cur_memcode').html(member);
+
+                // Hide the household section
+                $('#household').hide();
+
+                // Reveal the member section
+                $('#member').show();
+
+            });
+            // Remove the "save" button from the member row
+            btn.remove();
+            new_m_row.insertAfter(m_row);
+        }
+
+        // Remove unneccessary member rows:
+        if (!jQuery.isEmptyObject(form_data['members'])) {
+            let m_row = $('fieldset.h_member_row:nth-last-of-type(1)');
+            if (m_row.find('legend').html() == 'AAAS-NN') {
+                m_row.remove();
+            }
         }
     });
 
-    // Reveal the 'confirm passcode' field when 'create id' is clicked
-    $('#h_id_create').click(() => {
-        $('#h_id_pass_confirm')[0].parentNode.removeAttribute("style");
-    });
-
-    // Handle changes to household:identity:passcode
-    $('#h_id_pass').change((e) => {
-        form_data['household']['identity']['passcode'] = e.target.value;
-    });
-
-    // Handle changes to household:identity:confirm_passcode
-    $('#h_id_pass_confirm').change((e) => {
-        form_data['household']['identity']['confirm_passcode'] = e.target.value;
+    // Save location data to form data
+    $('#h_loc_save').click(() => {
+        form_data['household']['location']['country'] = $('#h_loc_country').val();
+        form_data['household']['location']['region'] = $('#h_loc_region').val();
+        form_data['household']['location']['city'] = $('#h_loc_city').val();
+        form_data['household']['location']['street_name'] = $('#h_loc_street').val();
+        form_data['household']['location']['postal_code'] = $('#h_loc_pcode').val();
     });
 
     // Add a new member row when 'add new member' is clicked
-    function saveMemberRow(memb_row) {
-
-        memb_row = $(memb_row);
-
-        // Grab input values
-        let m_age = memb_row.find('#h_mem_age').val();
-        let m_sex = memb_row.find('#h_mem_bio_gender').val();
-        let m_alias = memb_row.find('#h_mem_alias').val().toUpperCase();
-
-        // TODO: Validate input values
-        if (([m_age, m_sex].includes(null) || [m_age, m_sex].includes(''))) {
-            console.log("INVALID MEMBER ROW: " + m_age + ',' + m_sex + ',' + m_alias);
-            return false;
-        }
-
-        let memb_id = m_age + m_sex + '-' + m_alias;
-        if (memb_id in form_data['members']) {
-            console.log("DUPLICATE MEMBER EXISTS: " + memb_id);
-            return false;
-        }
-
-        let prev_memb_id = memb_row.find('legend').html();
-        if (prev_memb_id != "AAAS-NN") {
-            console.log("Swapping: " + prev_memb_id);
-            // We are altering a member. Preserve all data, then remove.
-            form_data['members'][memb_id] = form_data['members'][prev_memb_id];
-            delete form_data['members'][prev_memb_id];
-        } else {
-            form_data['members'][memb_id] = {};
-        }
-
-        // Save data.
-        form_data['members'][memb_id] = {
-            'age': m_age,
-            'sex': m_sex,
-            'alias': m_alias
-        };
-
-        // Update UI
-        memb_row.find('legend').html(memb_id);
-        memb_row.attr('id', memb_id);
-        let btn = memb_row.find('#h_mem_save');
-        btn.clone().attr('id', 'h_mem_delete').val('Delete').insertAfter(btn).click((e) => {
-            delMember(e.target.parentNode.parentNode.parentNode);
-        });
-        btn.clone().attr('id', 'h_mem_report').val('Report').insertAfter(btn).click(() => {
-            $('#m_cur_memcode').html(memb_id);
-            $('#member')[0].classList.toggle('hidden');
-            $('#household')[0].classList.toggle('hidden');
-        });
-        return true;
-    }
-
-    function delMember(memb_row) {
-        // TODO: prompt user for confirmation before deleting
-        memb_row = $(memb_row);
-
-        let prev_memb_id = memb_row.find('legend').html();
-
-        if (prev_memb_id == "AAAS-NN") {
-            memb_row.attr("id", 'AAAS-NN');
-            memb_row.find('#h_mem_age').val(null);
-            memb_row.find('#h_mem_sex').val(null);
-            memb_row.find('#h_mem_alias').val(null);
-        } else {
-            // Wipe data from form_data
-            let m_age = memb_row.find('#h_mem_age').val();
-            let m_sex = memb_row.find('#h_mem_bio_gender').val();
-            let m_alias = memb_row.find('#h_mem_alias').val().toUpperCase();
-            let memb_id = m_age + m_sex + '-' + m_alias;
-            if (memb_id in form_data['members']) {
-                delete form_data['members'][memb_id];
-            }
-            // Make sure not to remove the last slot, so that users can enter new members
-            if (jQuery.isEmptyObject(form_data['members'])) {
-                memb_row.attr({"id": 'AAAS-NN'});
-                memb_row.find('#h_mem_age').val(null);
-                memb_row.find('#h_mem_sex').val(null);
-                memb_row.find('#h_mem_alias').val(null);
-                memb_row.find('legend').html("AAAS-NN");
-                memb_row.find('#h_mem_delete').remove();
-                memb_row.find('#h_mem_report').remove();
-            } else {
-                memb_row.remove();
-            }
-        }
-    }
-
     $('#h_mem_save').click((e) => {
         saveMemberRow(e.target.parentNode.parentNode.parentNode);
     });
-
-    $('#h_mem_del').click((e) => {
-        delMember(e.target.parentNode.parentNode.parentNode)
-    });
-
     $('#h_mem_add').click(() => {
         let prv = $('fieldset.h_member_row:nth-last-of-type(1)');
-        let nxt = prv;
-        if (saveMemberRow(prv) || prv.find("legend").html() != "AAAS-NN") {
-            nxt = prv.clone().insertAfter(prv);
-            nxt.find('#h_mem_save').click((e) => {
+        if (prv.find("legend").html() != "AAAS-NN") {
+            let nxt = prv.clone();
+            nxt.insertAfter(prv);
+            prv = nxt;
+            let btn = nxt.find('#h_mem_delete');
+            btn.clone().attr('id', 'h_mem_save').val("Save").insertAfter(btn).click((e) => {
                 saveMemberRow(e.target.parentNode.parentNode.parentNode);
             });
         }
-        nxt.attr({"id": 'AAAS-NN'});
-        nxt.find('#h_mem_delete').remove();
-        nxt.find('#h_mem_report').remove();
-        nxt.find('#h_mem_age').val(null);
-        nxt.find('#h_mem_sex').val(null);
-        nxt.find('#h_mem_alias').val(null);
-        nxt.find('legend').html("AAAS-NN");
+        saveMemberRow(prv);
+        prv.attr({"id": 'AAAS-NN'});
+        prv.find('#h_mem_delete').remove();
+        prv.find('#h_mem_report').remove();
+        prv.find('#h_mem_age').val(null);
+        prv.find('#h_mem_sex').val(null);
+        prv.find('#h_mem_alias').val(null);
+        prv.find('legend').html("AAAS-NN");
+    });
+
+    // Delete Member row when 'Delete' is clicked
+    $('#h_mem_del').click((e) => {
+        delMember(e.target.parentNode.parentNode.parentNode)
     });
 
     $('#btnSubmit').click(() => {
@@ -211,10 +303,59 @@ $(document).ready(function () {
         $('#' + memb_id).css('background-color', 'var(--validated_data)');
         console.log(memb_id);
 
-        $('#member')[0].classList.toggle('hidden');
-        $('#household')[0].classList.toggle('hidden');
+        $('#member').hide();
+        $('#household').show();
     });
-    // Initiate a report for a household member
+
+    // Create a new profile
+    $('#h_id_create').click(() => {
+        // Remove any existing data
+        form_data = {
+            "household": {
+                "identity": {
+                    "unique_identifier": null,
+                    "passcode": null,
+                    "confirm_passcode": null
+                },
+                "location": {
+                    "country": null,
+                    "region": null,
+                    "city": null,
+                    "street_name": null,
+                    "postal_code": null
+                }
+            },
+            "members": {}
+        };
+
+        // Sort UI
+        $('form > .explainer').hide();
+        $('#h_location').show();
+        $('#h_id_load').hide();
+        $('#h_id_create').remove();
+
+        // Stub a new identity:
+        $('#h_id_uid').val("987-654-321");
+        $('#h_id_pass_confirm').parent().show();
+
+        // Hijack the location save button
+        let btn = $('#h_loc_save');
+        btn.clone().insertAfter(btn).val("Create Profile").click((e) => {
+            // TODO: Validate inputs
+            form_data['household']['identity']['unique_identifier'] = $('#h_id_uid').val();
+            form_data['household']['identity']['passcode'] = $('#h_id_pass').val();
+            form_data['household']['identity']['confirmed'] = $('#h_id_pass_confirm').val();
+            form_data['household']['location']['country'] = $('#h_id_loc_country').val();
+            form_data['household']['location']['region'] = $('#h_id_loc_region').val();
+            form_data['household']['location']['city'] = $('#h_id_loc_city').val();
+            form_data['household']['location']['street_name'] = $('#h_loc_street').val();
+            form_data['household']['location']['postal_code'] = $('#h_loc_pcode').val();
+            $(e.target).remove();
+            btn.show();
+            $('#h_id_load').click();
+        });
+        btn.hide();
+    });
 
 
 })
