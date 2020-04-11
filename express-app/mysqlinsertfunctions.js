d012d658cd4c4e70142f2e814b6aae10806f2dc8
Resident Six <gale.josh@gmail.com>
2020-04-11 09:23:37 -0400

    Code Cleanup

--- express-app/mysqlinsertfunctions.js
+++ express-app/mysqlinsertfunctions.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const config = require('./config');
+
 // ESTABLISH DATABASE CONNECTION
 const dbconn = mysql.createConnection({
     host: "localhost",
@@ -8,10 +9,12 @@ const dbconn = mysql.createConnection({
     database: config.CurveStomp.database,
     debug: false
 });
+
 dbconn.connect((err) => {
     if (err) throw err;
     console.log(" Db Connected!");
 });
+
 module.exports = {
     country: function addcountrydata(country) {
         return new Promise((resolve, reject) => {
@@ -21,9 +24,8 @@ module.exports = {
                     if (results.length > 0) {
                         resolve(results[0]['ID']);
 
-                    }
-                    else {
-                        dbconn.query('insert into country(iso_code)values("' + country + '")', (err, results) => {
+                    } else {
+                        dbconn.query('insert into country(iso_code)values(?)', country, (err, results) => {
                             if (err) throw err;
                             resolve(results.insertId);
                         });
@@ -31,16 +33,14 @@ module.exports = {
                     }
                 });
 
-            }
-            else {
+            } else {
                 dbconn.query("select ID from country where name=?", country, (err, results) => {
                     if (err) throw err;
                     if (results.length > 0) {
                         resolve(results[0]['ID']);
 
-                    }
-                    else {
-                        dbconn.query('insert into country(name)values("' + country + '")', (err, results) => {
+                    } else {
+                        dbconn.query('insert into country(name)values(?)', country, (err, results) => {
                             if (err) throw err;
                             resolve(results.insertId);
                         });
@@ -52,34 +52,7 @@ module.exports = {
         })
 
 
-
     },
-
-    //Function to add city for create profile
-    city: function addcity(city) {
-        return new Promise((resolve, reject) => {
-            dbconn.query("select ID from city where name=?", city, (err, results) => {
-                if (err) throw err;
-                if (results.length > 0) {
-                    resolve(results[0]['ID']);
-
-                }
-                else {
-                    dbconn.query('insert into city(name)values("' + city + '")', (err, results) => {
-                        if (err) throw err;
-                        resolve(results.insertId);
-                    });
-
-                }
-            });
-        })
-
-
-    },
-
-
-    //Function to add region for create profile
-
     region: function addregion(region) {
         return new Promise((resolve, reject) => {
 
@@ -88,9 +61,8 @@ module.exports = {
                 if (results.length > 0) {
                     resolve(results[0]['ID']);
 
-                }
-                else {
-                    dbconn.query('insert into region(name)values("' + region + '")', (err, results) => {
+                } else {
+                    dbconn.query('insert into region(name)values(?)', region, (err, results) => {
                         if (err) throw err;
                         resolve(results.insertId);
                     });
@@ -99,38 +71,58 @@ module.exports = {
             });
 
         })
+    },
+    city: function addcity(city) {
+        return new Promise((resolve, reject) => {
+            dbconn.query("select ID from city where name=?", city, (err, results) => {
+                if (err) throw err;
+                if (results.length > 0) {
+                    resolve(results[0]['ID']);
+
+                } else {
+                    dbconn.query('insert into city(name)values(?)', city, (err, results) => {
+                        if (err) throw err;
+                        resolve(results.insertId);
+                    });
+
+                }
+            });
+        })
 
 
     },
-    location: function(country,region,city,street_name,postal_code)
-    {
-        return new Promise((resolve,reject)=>{
-             dbconn.query('insert into location(country,region,city,street_name,postal_code)values("' + country + '", "' + region + '", "' + city + '", "' + street_name + '", "' + postal_code + '")',(err,results)=>{
-            if(err) throw err;
-            resolve(results.insertId);        
-        });
+    location: function (country, region, city, street_name, postal_code) {
+        return new Promise((resolve, reject) => {
+            dbconn.query(
+                'insert into location(country,region,city,street_name,postal_code)values(?,?,?,?,?)',
+                [country, region, city, street_name, postal_code],
+                (err, results) => {
+                    if (err) throw err;
+                    resolve(results.insertId);
+                });
         })
     },
-    household: function(uid,passcode)
-    {
-        return new Promise((resolve,reject)=>{
-            dbconn.query('insert into household(identifier,passcode)values("' + uid + '", "' + passcode + '")',(err,results)=>{
-                if(err) throw err;
-                resolve(results.insertId);
-            })
+    household: function (uid, passcode) {
+        return new Promise((resolve, reject) => {
+            dbconn.query(
+                'insert into household(identifier,passcode)values(?,?)',
+                [uid, passcode],
+                (err, results) => {
+                    if (err) throw err;
+                    resolve(results.insertId);
+                })
         })
     },
-    household_location: function(household,location)
-    {
-        return new Promise((resolve,reject)=>{
-            dbconn.query('insert into household_location(household_id,location_id)values("' + household + '", "' + location + '")',(err,results)=>{
-                if(err) throw err;
-                resolve("Profile Created");
-            })
+    household_location: function (household, location) {
+        return new Promise((resolve, reject) => {
+            dbconn.query(
+                'insert into household_location(household_id,location_id)values(?,?)',
+                [household, location], (err, results) => {
+                    if (err) throw err;
+                    resolve("Profile Created");
+                })
         })
     }
-    
-
-}
+};
 
 
--- express-app/server-dev.js
+++ express-app/server-dev.js
@@ -3,7 +3,6 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
-
 const config = require('./config');
 const mysqlinsert = require('./mysqlinsertfunctions');
 const mysqlselect = require('./mysqlselectfunctions');
@@ -17,19 +16,6 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-
-// // ESTABLISH DATABASE CONNECTION
-// const dbconn = mysql.createConnection({
-//     host: "localhost",
-//     user: config.CurveStomp.user,
-//     password: config.CurveStomp.pass,
-//     database: config.CurveStomp.database,
-//     debug: false
-// });
-// dbconn.connect((err) => {
-//     if (err) throw err;
-//     console.log(" Db Connected!");
-// });
 
 // CONFIGURE WEB SERVER
 var http = require("http").Server(app);
@@ -50,175 +36,32 @@ function isAuthenticated(req, res, next) {
 
 // CONFIGURE UI ROUTING
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('form');
 });
 app.get('/form', (req, res) => {
     res.render('index');
 });
-app.get('/household', isAuthenticated, (req, res) => {
-    var guid = req.session.uid;
-    var firstpart = guid.slice(0, 3);
-    var secondpart = guid.slice(3, 6);
-    var thirdpart = guid.slice(6, 9);
-    var hid = firstpart + "-" + secondpart + "-" + thirdpart;
-    res.render('household', {guid: hid});
-});
-app.get('/form2', (req, res) => {
-    res.render('form2');
-});
-app.get('/homepage/?', (req, res) => {
-    res.render('homepage');
-});
+// app.get('/household', isAuthenticated, (req, res) => {
+//     var guid = req.session.uid;
+//     var firstpart = guid.slice(0, 3);
+//     var secondpart = guid.slice(3, 6);
+//     var thirdpart = guid.slice(6, 9);
+//     var hid = firstpart + "-" + secondpart + "-" + thirdpart;
+//     res.render('household', {guid: hid});
+// });
+// app.get('/form2', (req, res) => {
+//     res.render('form2');
+// });
+// app.get('/homepage/?', (req, res) => {
+//     res.render('homepage');
+// });
 
-// CONFIGURE API ROUTING
-// app.get('/gethouseholdid', (req, res) => {
-//     var hid = Math.floor(Math.random() * 900000000) + 100000000;
-//     dbconn.query('select household_guid from household where household_guid=?', [hid], (error, results) => {
-//         if (error) throw error;
-//         if (results.lenght == 0) {
-//             res.send(hid.toString());
-//         } else {
-//             var hid = Math.floor(Math.random() * 900000000) + 100000000;
-//             res.send(hid.toString());
-//         }
-//     });
-// });
-// app.get('/countrylist', (req, res) => {
-//     res.send(countryjson);
-// });
-// app.post('/login', (req, res) => {
-//     var exidn = req.body.identity;
-//     var expass = req.body.pass;
-//     console.log(exidn);
-//     console.log(expass);
-// });
-// app.post('/report.symptom', (req, res) => {
-//     var user_guid = req.body.user_guid;
-//     var member_id = req.body.member_id;
-//     var dry_cough = req.body.dry_cough;
-//     var pneumonia = req.body.pneumonia;
-//     var difficulty_breathing = req.body.difficulty_breathing;
-//     var difficulty_walking = req.body.difficulty_walking;
-//     var appetite = req.body.appetite;
-//     var diarrhea = req.body.diarrhea;
-//     var muscle_ache = req.body.muscle_ache;
-//     var fatigue = req.body.fatigue;
-//     var runny_nose = req.body.runny_nose;
-//     var congestion = req.body.congestion;
-//     var sore_throat = req.body.sore_throat;
-//     var fever_c = req.body.fever_c;
-//     var headache = req.body.headache;
-//     var confusion_dizziness = req.body.confusion_dizziness;
-//     var nausea = req.body.nausea;
-//     var chills = req.body.chills;
-//     var other_pain = req.body.other_pain;
-// });
-// app.post(['/createhouseholdprofile', '/homepage/createhouseholdprofile'], (req, res) => {
-//     var huid = req.body.huid;
-//     console.log(req.body);
-//     var pass = req.body.pass;
-//     var country = req.body.country;
-//     var region = req.body.region;
-//     var city = req.body.city;
-//     var street = req.body.street_name;
-//     var postal_code = req.body.postal_code;
-//     req.session.loggedin = true;
-//     req.session.uid = huid;
-//     // request.session.username = username;
-//     res.redirect('/household');
-//
-// });
-// app.post('/report.testing', (req, res) => {
-//     var user_guid = req.body.user_guid;
-//     var tested = req.body.tested;
-//     var recovered = req.body.recovered;
-//     var hospitalized = req.body.hospitalized;
-//     var ventilation = req.body.ventilation;
-//     var oxygen = req.body.oxygen;
-//     var other_symptoms = req.body.other_symptoms;
-//
-// });
-// app.post('/report.transmission', (req, res) => {
-//     var user_guid = req.body.user_guid;
-//     var travel_amount = req.body.travel_amount;
-//     var travel_distance = req.body.travel_distance;
-//     var hospitalized = req.body.hospitalized;
-//     var surface_touch = req.body.surface_touch;
-//     var number_of_regular_contacts = req.body.number_of_regular_contacts;
-//
-// });
-// app.post('/register.user', (req, res) => {
-//     var user_guid = req.body.user_guid;
-//     var passcode = req.body.passcode;
-//     var user_guid_value = req.body.user_guid_value;
-//     var passcode_value = req.body.passcode_value;
-//
-// });
-// app.post('/authenticate.user', (req, res) => {
-//     var user_guid = req.body.user_guid;
-//     var user_guid_value = req.body.user_guid_value;
-//     var passcode = req.body.passcode;
-//     var passcode_value = req.body.passcode_value;
-//
-// });
-// app.post('/createuserprofile', (req, res) => {
-//     var usremail = req.body.usremail;
-//     var usridn = req.body.usridn;
-//     var usrpass = req.body.usrpass;
-//     var usr_total_mem = req.body.usr_total_mem;
-//     var usr_country = req.body.usr_country;
-//     var usr_region = req.body.usr_region;
-//     var usr_city = req.body.usr_city;
-//     var usr_street = req.body.usr_street;
-//     var usr_pcode = req.body.usr_pcode;
-//     var usr_age = req.body.usr_age;
-//     var usr_gender = req.body.usr_gender;
-//     var usr_cough = req.body.usr_cough;
-//     var usr_cough_prod = req.body.usr_cough_prod;
-//     var usr_pneumonia = req.body.usr_pneumonia;
-//     var usr_breathing = req.body.usr_breathing;
-//     var usr_walking = req.body.usr_walking;
-//     var usr_appetite = req.body.usr_appetite;
-//     var usr_diarrhea = req.body.usr_diarrhea;
-//     var usr_musclepain = req.body.usr_musclepain;
-//     var usr_fatigue = req.body.usr_fatigue;
-//     var usr_nose = req.body.usr_nose;
-//     var usr_fever = req.body.usr_fever;
-//     var usr_headache = req.body.usr_headache;
-//     var usr_dizziness = req.body.usr_dizziness;
-//     var usr_nausea = req.body.usr_nausea;
-//     var usr_chills = req.body.usr_chills;
-//     var usr_gpain = req.body.usr_gpain;
-//     var usr_trans_isolation = req.body.usr_trans_isolation;
-//     var usr_trans_distance = req.body.usr_trans_distance;
-//     var usr_trans_surface = req.body.usr_trans_surface;
-//     var usr_trans_human = req.body.trans_human;
-//
-//     console.log(req.body);
-//     dbconn.query('insert into household(household_guid) values(?)', [usridn], (err, res) => {
-//         if (err) throw err;
-//         var rowid = res.insertId;
-//     });
-//
-//
-//     res.send("Record Submitted");
-// });
-// app.post('/register.location', (req, res) => {
-//     var user_guid = req.body.user_guid;
-//     var user_guid_value = req.body.user_guid_value;
-//     var passcode = req.body.passcode;
-//     var passcode_value = req.body.passcode_value;
-//
-// });
-//
+let max_hid = 0;
 
-// TODO: REMOVE FROM PRODUCTION SERVER....
-
-let max_hid=0;
-async function maxid()
-{
-    max_hid=await(mysqlselect.maxid());
+async function maxid() {
+    max_hid = await (mysqlselect.maxid());
 }
+
 maxid(); // TODO: update max_hid on startup with largest PK in DB-DONE.
 const cors = require('cors'); // TODO: Consider removing for production
 app.use(cors({origin: '*'})); // TODO: Consider removing for production
@@ -320,6 +163,7 @@ app.post('/api/create_profile/?', ValidationRules.create_profile(), validate, as
     // TODO: validate data received
 
 });
+
 // FIRE UP SERVER
 const PORT = 37248;
 http.listen(PORT, function () {
